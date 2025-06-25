6e9520d20acfb7bb8f0ee983d9f361c620ab6947
awibis20 <andrawibisono5@gmail.com>
2025-06-25 17:04:46 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,2 +1,22 @@
-const { getMatchingProduct } = require('./keywords');
+const { queryAirtable } = require("./airtable");
+
+function buildFormulaFromTags(tags) {
+  const clauses = [];
+
+  for (const [field, values] of Object.entries(tags)) {
+    for (const value of values) {
+      clauses.push(`NOT(ISERROR(SEARCH("${value}", {${field}})))`);
+    }
+  }
+
+  return `AND(${clauses.join(",")})`;
+}
+
+async function getMatchingProduct(tags) {
+  const formula = buildFormulaFromTags(tags);
+  const products = await queryAirtable(formula);
+
+  return { products }; // or filter/select the best one here
+}
+
 module.exports = { getMatchingProduct };
