00eb6fb2e49962523634017425ac9ed07a6cb772
awibis20 <andrawibisono5@gmail.com>
2025-06-26 10:13:43 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,26 +1,11 @@
-const { queryAirtable } = require('./airtable');
-
-function buildFormula(requests) {
-  const blocks = requests.map(req => {
-    const conditions = [];
-    conditions.push(`NOT(ISERROR(SEARCH("${req.Object_Type}", {Object_Type})))`);
-
-    Object.entries(req).forEach(([field, values]) => {
-      if (field === 'Object_Type') return;
-      values.forEach(value => {
-        conditions.push(`NOT(ISERROR(SEARCH("${value}", {${field}})))`);
-      });
-    });
-
-    return `AND(${conditions.join(', ')})`;
+function buildFormula(tagGroups) {
+  const formulaBlocks = tagGroups.map(group => {
+    const subConditions = Object.entries(group)
+      .map(([field, value]) => `SEARCH("${value}", {${field}})`);
+    return `AND(${subConditions.join(', ')})`;
   });
-
-  return `OR(${blocks.join(', ')})`;
+  return `OR(${formulaBlocks.join(', ')})`;
 }
 
-async function getFilteredProducts(payload) {
-  const formula = buildFormula(payload.requests);
-  return await queryAirtable(formula);
-}
+module.exports = { buildFormula };
 
-module.exports = { getFilteredProducts };
