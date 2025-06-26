0be97f49cd5711adde53b0769eac1242ac5c531a
awibis20 <andrawibisono5@gmail.com>
2025-06-26 08:31:12 +0700

    Update airtable.js

--- airtable.js
+++ airtable.js
@@ -1,35 +1,39 @@
-const axios = require("axios");
-require("dotenv").config();
-
-const baseId = process.env.AIRTABLE_BASE_ID;
-const tableId = process.env.AIRTABLE_TABLE_ID;
-const apiKey = process.env.AIRTABLE_API_KEY;
+// === FILE 1: airtable.js ===
+const axios = require('axios');
 
 async function queryAirtable(formula) {
-  const encodedFormula = encodeURIComponent(formula);
-  const url = `https://api.airtable.com/v0/${baseId}/${tableId}?filterByFormula=${encodedFormula}`;
+  const baseId = process.env.AIRTABLE_BASE_ID;
+  const tableId = process.env.AIRTABLE_TABLE_ID;
+  const apiKey = process.env.AIRTABLE_API_KEY;
 
-  const config = {
+  const encodedFormula = encodeURIComponent(formula);
+
+  const fields = [
+    'Image_URL',
+    'Product_Name',
+    'Object_Type',
+    'Color_Material',
+    'Form',
+    'Design_Language',
+    'Emotional_Character',
+    'Design_Summary'
+  ];
+
+  const url = `https://api.airtable.com/v0/${baseId}/${tableId}?filterByFormula=${encodedFormula}` +
+    fields.map(field => `&fields[]=${encodeURIComponent(field)}`).join('');
+
+  const response = await axios.get(url, {
     headers: {
       Authorization: `Bearer ${apiKey}`
-    },
-    params: {
-      fields: [
-        "Product_Name",
-        "Product_URL",
-        "Image_URL",
-        "Color_Material",
-        "Form",
-        "Object_Type",
-        "Design_Language",
-        "Emotional_Character",
-        "Design_Summary"
-      ]
     }
-  };
+  });
 
-  const response = await axios.get(url, config);
-  return response.data.records.map(record => record.fields);
+  return {
+    formula_used: formula,
+    encoded_formula: encodedFormula,
+    records: response.data.records
+  };
 }
 
 module.exports = { queryAirtable };
+
